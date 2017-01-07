2d6e126c57da41260b063605aeaccbd0f1cdeb9b
Oleksandr <avpavlov96@gmail.com>
2017-01-07 18:24:24 +0300

    WebAppConfig added

---
+++ src/main/java/ua/alexandroid1/alex/config/WebAppConfig.java
@@ -0,0 +1,33 @@
+package ua.alexandroid1.alex.config;
+
+/**
+ * Created by Oleksandr on 07.01.2017.
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+// Говорим, что это конфигурация
+@Configuration
+// Включаем MVC
+@EnableWebMvc
+// Указываем где искать контроллеры и остальные компоненты
+@ComponentScan("ua.alexandroid1.alex")
+public class WebAppConfig {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        // указываем где будут лежать наши веб-страницы
+        resolver.setPrefix("/pages/");
+        // формат View который мы будем использовать
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+
+}
